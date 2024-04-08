b7ffef07c84f3f90cfb2bf033211df81a863f96e
audrey cherop <audreycherop06@gmail.com>
2024-04-08 12:19:53 +0300

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const cleanWord = word.replace(/[^a-zA-Z]/g, '').toLowerCase();
+
+  return cleanWord === cleanWord.split('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+  remove any non letter characters from the word and convertthem to lowercase.
+  check if cleaned word is equal to its reverse
+  return true if they are equal
+  return false if otherwise
 */
 
 /*
   Add written explanation of your solution here
+  the first function removes any non letters from the input words and converts them to lowercase.
+  then it checks if the cleaned word is equal to its reverse .if they are equal it returns true indicating the word is a palindrome and if not it returns false
+  showing the word is not a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
